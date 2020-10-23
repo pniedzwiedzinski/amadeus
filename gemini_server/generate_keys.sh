13f84ddd23d58bc1ae19faf2b8b5b98f907f67cd
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-10-23 20:22:14 +0200

    Add ssl config

--- gemini_server/generate_keys.sh
+++ gemini_server/generate_keys.sh
@@ -2,5 +2,5 @@
 
 keysdir="$HOME/.gmi.keys"
 
-openssl req -x509 -newkey rsa:4096 -keyout $keysdir/ca-key.rsa -out $keysdir/ca-cert.pem \
-	    -days 3650 -nodes -subj "/CN=o72yu1cbgzortw3f.myfritz.net"
+openssl req -new -x509 -newkey rsa:2048 -sha256 -nodes -keyout $keysdir/ca-key.rsa -out $keysdir/ca-cert.pem \
+	    -days 3650 -config $HOME/amadeus/gemini_server/ssl.conf
